45a98b3f76460df361c6c7e6e60279ac12fdcc3d
unknown <Prachaya>
2025-03-24 14:56:59 +0700

    post assignment

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,52 @@
+import { useState } from "react";
+import { postData } from "../raw-data/post-data";
+
 function Posts() {
+  const [postList,setPostList] = useState(postData)
+
+  const addLike = (postId) => {
+    const newPost = postList.map((post) => {
+      if (post.id === postId) {
+        return {...post, likes: post.likes + 1}
+      }
+      return post
+    })
+    setPostList(newPost)
+  }
+
+  const dislike = (postId) => {
+    const newPost = postList.map((post) => {
+      if (post.id === postId && post.likes > 0) {
+        return {...post, likes: post.likes - 1}
+      }
+      return post
+    })
+    setPostList(newPost)
+  }
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+      {postList.map((post) => {
+        return (
+          <div class="post-item" key={post.id}>
+            <div class="post-header">
+              <h2>{post.title}</h2>
+              <div class="post-social-media-stats">
+                <span class="stats-topic">Likes: </span>
+                <span class="post-likes">{post.likes}</span>
+              </div>
+            </div>
+            <p class="post-content">
+              {post.content}
+            </p>
+            <div class="post-actions">
+              <button class="like-button" onClick={() => addLike(post.id)}>Like</button>
+              <button class="dislike-button" onClick={() => dislike(post.id)}>Dislike</button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+        )
+    })}
       </div>
     </div>
   );
